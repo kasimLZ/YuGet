a961a94d395562bbefbb15f0c5a5051dd044265a
kasim <liuzhe_kasim@outlook.com>
2021-07-20 19:42:42 +0800

    package list page

---
+++ src/YuGet.Ui/Pages/Packages.razor.cs
@@ -0,0 +1,22 @@
+﻿using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YuGet.Database.Models;
+
+namespace YuGet.Ui.Pages
+{
+	public class PackagesComponent : LayoutComponentBase
+	{
+		protected IList<Package> PackageList { get; private set; } = new List<Package>();
+
+		protected int PageSize = 20;
+
+		protected void OnPage(MatPaginatorPageEvent e)
+		{
+			PackageList.Add(new Package());
+		}
+	}
+}
